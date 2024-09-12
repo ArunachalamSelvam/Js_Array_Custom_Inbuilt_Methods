87542d8dc53c4f6cb6389e0b871632cea696847d
ArunachalamSelvam <159519518+ArunachalamSelvam@users.noreply.github.com>
2024-09-12 06:44:25 +0530

    Update arrayRemoveMethod.js

--- arrayRemoveMethod.js
+++ arrayRemoveMethod.js
@@ -1,5 +1,7 @@
 Array.prototype.remove=function(index){
-
+    if (typeof index !== 'number' || index < 0 || index >= this.length) {
+        throw new RangeError('Invalid index');
+    }
     if(this.length==0){
         return this;
     }    
