1025b2253e696bf14feb8fa77084b2def41245a2
ArunachalamSelvam <159519518+ArunachalamSelvam@users.noreply.github.com>
2024-09-12 06:53:16 +0530

    Create arrayRemoveIndexMethod.js

---
+++ arrayRemoveIndexMethod.js
@@ -0,0 +1,13 @@
+Array.prototype.remove=function(index){
+    if (typeof index !== 'number' || index < 0 || index >= this.length) {
+        throw new RangeError('Invalid index');
+    }
+    if(this.length==0){
+        return this;
+    }    
+    for(let i=index;i<this.length-1;i++){
+        this[i]=this[i+1];
+    }
+    this.length = this.length-1;
+    return this;
+}
